6431ed36143bcc3faa5545bcd93b4175b9e75f9c
trungct <trungcaot@hotmail.com>
2019-05-06 15:38:25 +0700

    LoginPage is added with some animation classes and value converters.

---
+++ src/SmileWpf/SmileWpf/Pages/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmileWpf
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : BasePage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.AnimateOut();
+        }
+    }
+}
